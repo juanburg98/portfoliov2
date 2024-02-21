1c1355d83a8c644d7522cc5642b8a3e24dbbe22a
Juan Burgueño <juanburg98@gmail.com>
2024-02-21 11:36:26 -0300

    defaultTheme added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        serif: ['"Playfair Display", sans serif'],
+      },
+      colors: {
+        primary: "#171717", // title
+        secondary: "#666666", // text
+        tertiary: "#fafafa", // bg
+        quaternary: "#ebebeb", // hover
+        quinary: "#fcfcf7", // hover
+        sextenary: "#0070f3", // detail
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      letterSpacing: {
+        title: "-0.80px",
+        text: "-0.2px",
+      },
+    },
+  },
+  plugins: [],
+};
