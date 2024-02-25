f9e57665be91cf22c9b71925dcb4ec0a1f3fbe53
Juan Burgueño <juanburg98@gmail.com>
2024-02-25 14:27:46 -0300

    Project restructuring

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,9 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
         serif: '"Source Code Pro", monospace',
       },
       colors: {
